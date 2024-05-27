7e6357578194466b9d581c7bd64721a99524a419
sumitchavann <136459557+sumitchavann@users.noreply.github.com>
2024-05-27 14:02:49 +0530

    Chnages
    Done

--- portfolio/activity.js
+++ portfolio/activity.js
@@ -6,64 +6,63 @@ const connection = require('../db');
 router.get('/', (req, res) => {
     console.log("Received GET request");
 
+    // Your specific data
+    const activityData = [
+     
+     
+[
+    {
+      "activityType": "REACTION",
+      "type": "CLAPPING",
+      "count": 2,
+      "createdAt": "2024-05-27T03:36:18.679Z",
+      "slug": "id-tailwindcss-best-practices",
+      "contentTitle": "Tailwind CSS Best Practices",
+      "contentType": "POST"
+    },
+    {
+      "activityType": "REACTION",
+      "type": "CLAPPING",
+      "count": 1,
+      "createdAt": "2024-05-27T03:36:18.064Z",
+      "slug": "id-tailwindcss-best-practices",
+      "contentTitle": "Tailwind CSS Best Practices",
+      "contentType": "POST"
+    },
+    {
+      "activityType": "REACTION",
+      "type": "AMAZED",
+      "count": 1,
+      "createdAt": "2024-05-27T03:36:17.192Z",
+      "slug": "id-tailwindcss-best-practices",
+      "contentTitle": "Tailwind CSS Best Practices",
+      "contentType": "POST"
+    },
+    {
+      "activityType": "REACTION",
+      "type": "AMAZED",
+      "count": 1,
+      "createdAt": "2024-05-27T03:36:16.570Z",
+      "slug": "id-tailwindcss-best-practices",
+      "contentTitle": "Tailwind CSS Best Practices",
+      "contentType": "POST"
+    },
+    {
+      "activityType": "REACTION",
+      "type": "AMAZED",
+      "count": 1,
+      "createdAt": "2024-05-27T03:36:14.411Z",
+      "slug": "id-tailwindcss-best-practices",
+      "contentTitle": "Tailwind CSS Best Practices",
+      "contentType": "POST"
+    }
+  ]
+    ];
 
-    activityData = [
-        {
-            "activityType": "REACTION",
-            "type": "CLAPPING",
-            "count": 2,
-            "createdAt": "2024-05-27T03:36:18.679Z",
-            "slug": "id-tailwindcss-best-practices",
-            "contentTitle": "Tailwind CSS Best Practices",
-            "contentType": "POST"
-        },
-        {
-            "activityType": "REACTION",
-            "type": "CLAPPING",
-            "count": 1,
-            "createdAt": "2024-05-27T03:36:18.064Z",
-            "slug": "id-tailwindcss-best-practices",
-            "contentTitle": "Tailwind CSS Best Practices",
-            "contentType": "POST"
-        },
-        {
-            "activityType": "REACTION",
-            "type": "AMAZED",
-            "count": 1,
-            "createdAt": "2024-05-27T03:36:17.192Z",
-            "slug": "id-tailwindcss-best-practices",
-            "contentTitle": "Tailwind CSS Best Practices",
-            "contentType": "POST"
-        },
-        {
-            "activityType": "REACTION",
-            "type": "AMAZED",
-            "count": 1,
-            "createdAt": "2024-05-27T03:36:16.570Z",
-            "slug": "id-tailwindcss-best-practices",
-            "contentTitle": "Tailwind CSS Best Practices",
-            "contentType": "POST"
-        },
-        {
-            "activityType": "REACTION",
-            "type": "AMAZED",
-            "count": 1,
-            "createdAt": "2024-05-27T03:36:14.411Z",
-            "slug": "id-tailwindcss-best-practices",
-            "contentTitle": "Tailwind CSS Best Practices",
-            "contentType": "POST"
-        }
-]
-
-
-    res.status(200).json({
-      status: 1,
-      response: 'data feteched  successfully.',
-      data : activityData   
-    });
-  } );
-
+    // Return the specific data
+    res.status(200).json(
+        activityData
+    );
+});
 
 module.exports = router;
-
-
