6b5868bfa18a58701d44bd132bce0cd8e6fa1a0a
sumitchavann <136459557+sumitchavann@users.noreply.github.com>
2024-05-27 14:34:15 +0530

    chnages
    Done

--- api.js
+++ api.js
@@ -56,8 +56,18 @@ const PORT = process.env.PORT || 5000;
 //   next(); 
 // }
 
-// app.use(myMiddleware);
 
+app.use((req, res, next) => {
+  // Allow requests from any origin
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Set allowed methods
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  // Set allowed headers
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // Allow credentials
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.get('/', (req, res) => {  
     res.send('server is connected');
